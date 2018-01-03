48290c45c8c5a34876f83df562a2105922acd788
jrmedd <hello@jamesmedd.co.uk>
2018-01-03 16:57:46 +0000

    Tidy up

--- web_app.py
+++ web_app.py
@@ -1,37 +1,37 @@
-from flask import Flask, url_for, request, render_template, jsonify
+import os
+from flask import Flask, request, render_template
 import requests
 from bs4 import BeautifulSoup
-import os
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-tfgm_api_key = os.environ.get('TFGM_API') #API subscription key available from developer.tfgm.com
-api_header = {'Ocp-Apim-Subscription-Key':tfgm_api_key} #the http header for requesting data
+TFGM_API_KEY = os.environ.get('TFGM_API') #API subscription key available from developer.tfgm.com
+API_HEADER = {'Ocp-Apim-Subscription-Key':TFGM_API_KEY} #the http header for requesting data
 
-@app.route('/display/<stop>')
+@APP.route('/display/<stop>')
 def display(stop):
-    return render_template('display.html',stop=stop,timetable=fetch_timetable(stop))
+    return render_template('display.html', stop=stop, timetable=fetch_timetable(stop))
 
-@app.route('/timetable/<stop>')
+@APP.route('/timetable/<stop>')
 def fetch_timetable(stop):
     if request.args.get('api'):
-        timetable=api_timetable(stop)
+        timetable = api_timetable(stop)
     else:
-        timetable=scrape_timetable(stop)
+        timetable = scrape_timetable(stop)
     return timetable
 
 def scrape_timetable(stop):
     stop_url = "https://tfgm.com/public-transport/tram/stops/%s-tram" % (stop.lower().replace(' ', '-')) #TFGM's url convention for tram stops
     page = requests.get(stop_url) #load the page
     soup = BeautifulSoup(page.text, "lxml") #parse the HTML
-    table = soup.find('table',attrs={'id':'departures-data'}) #find the departures table
+    table = soup.find('table', attrs={'id':'departures-data'}) #find the departures table
     table_body = table.find('tbody') #grab the table's body
     return table_body #return it as a string
 
 def api_timetable(stop):
-    results = requests.get("https://api.tfgm.com/odata/Metrolinks?$filter=StationLocation eq '%s'" % (stop),headers=api_header) #get all Metrolink board times
+    results = requests.get("https://api.tfgm.com/odata/Metrolinks?$filter=StationLocation eq '%s'" % (stop), headers=API_HEADER) #get all Metrolink board times
     boards = results.json().get('value') #create an list of boards per stop
-    boards = {v.get('AtcoCode'):v for v in boards}.values() #filter out duplicate boards per platform using the Atco codes
+    boards = {board.get('AtcoCode'):board for board in boards}.values() #filter out duplicate boards per platform using the Atco codes
     table = "" #create empty table string to pass to browser
     for board in boards:
         table += '<tbody>'#start a new tbody per board
@@ -48,4 +48,4 @@ def api_timetable(stop):
     return table
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    APP.run(host="0.0.0.0", debug=True)
